876d9e21916bb2e8cac251d3b8912cc88098b52a
gustavolasantos-boop <gustavolasantos@gmail.com>
2026-04-09 10:58:39 -0300

    aula

---
+++ aula02/aula02.py
@@ -0,0 +1,38 @@
+num1 = 4
+num2 = 2
+
+print(type(num1), type (num2))
+
+operacao = num1 / num2
+print(operacao, type(operacao))
+
+#Operadores de atribuição
+num=15
+print()#pular linha
+print(num)
+
+num = num + 2
+print(num)
+
+num = num / 2
+print(num)
+
+#operadores relacionais
+print()
+print(6 >= 6)
+
+idade = 22
+
+print(idade >= 21)
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade, type(maior_idade))
+
+#Strings
+nome1= "Marcos"
+nome2 = "Marcos"
+
+print(nome1.upper() == nome2.upper())
---
+++ aula3/aula03.py
@@ -0,0 +1,13 @@
+import math
+
+num =17
+raiz = math.sqrt(num)
+print(f"A raiz de {num} é {raiz:.2f}")
+
+import random
+
+num_random = random.random()
+print(num_random*100)
+
+num_rand_int = random.randint(1,10)
+print(num_rand_int)
